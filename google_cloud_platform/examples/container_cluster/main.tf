09e259c058568b41c1611d0c214666a1ae9b3004
Vitalii Natarov <vitalii_natarov@epam.com>
2018-07-27 15:29:16 +0300

    Added compute_firewall to this module

--- google_cloud_platform/examples/container_cluster/main.tf
+++ google_cloud_platform/examples/container_cluster/main.tf
@@ -12,6 +12,20 @@ provider "google" {
     region      = "us-east1"
 }   
 
+module "compute_firewall" {
+    source                          = "../../modules/compute_firewall"
+    name                            = "TEST"
+
+    project                         = "terraform-2018"
+
+    enable_all_ingress              = true    
+    enable_all_egress               = true
+
+    #enable_all_ingress              = false
+    #allow_protocol                  = "icmp"
+    #allow_ports                     = ["80", "443"]
+}
+
 module "container_cluster" {
     source                              = "../../modules/container_cluster"
     name                                = "TEST"
@@ -19,6 +33,7 @@ module "container_cluster" {
     # Create container cluster in zone
     enable_container_cluster            = true
     zone                                = "us-east1-b"
+    additional_zones                    = []
     region                              = ""
     #    
     # Create container cluster in region
@@ -34,7 +49,7 @@ module "node_pool" {
     enable_container_cluster            = false
     
     # Create container node pool
-    enable_container_node_pool          = true
+    enable_container_node_pool          = false
     container_cluster_name              = "${element(module.container_cluster.google_container_cluster_zone_name, 0)}"
     ##OR
     ##container_cluster_name              = "${element(module.container_cluster.google_container_cluster_region_name, 0)}"
