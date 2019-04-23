916a63ca007ab29edf03823b50ed52fd206c2df7
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-04-23 16:35:54 +0300

    Added life circle

--- newrelic/modules/newrelic_alert/newrelic_alert_policy_channel.tf
+++ newrelic/modules/newrelic_alert/newrelic_alert_policy_channel.tf
@@ -3,4 +3,13 @@ resource "newrelic_alert_policy_channel" "alert_policy_channel" {
             
     policy_id   = "${var.alert_policy_policy_id}"
     channel_id  = "${var.alert_policy_channel_id}"
+
+    lifecycle = {
+        create_before_destroy   = true,
+        ignore_changes          = []
+    }
+  
+    depends_on  = []
+
 }
+
