41a6861f553acb26badd2cf31e308d514e67bd41
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-08-01 15:40:30 +0300

    Fix human issue

--- aws/modules/key_pair/key_pair.tf
+++ aws/modules/key_pair/key_pair.tf
@@ -2,7 +2,7 @@
 # Create AWS key pair
 #---------------------------------------------------
 resource "aws_key_pair" "key_pair_key_name" {
-    count           = "${var.enable_key_pair && length(var.key_name) !="" ? 1 : 0}"
+    count           = "${var.enable_key_pair && var.key_name != "" ? 1 : 0}"
     
     key_name        = "${var.key_name}"
     public_key      = "${var.public_key}"
@@ -17,7 +17,7 @@ resource "aws_key_pair" "key_pair_key_name" {
 }
 
 resource "aws_key_pair" "key_pair_key_name_prefix" {
-    count           = "${var.enable_key_pair && length(var.key_name) =="" ? 1 : 0}"
+    count           = "${var.enable_key_pair && length(var.key_name) == 0 ? 1 : 0}"
 
     key_name_prefix = "${var.key_name_prefix}"
     public_key      = "${var.public_key}"
