e4222865088969cb6213072789a78cb721ca463d
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-11-18 02:10:43 +0200

    Migrate to tf-0.12

--- aws/examples/acm/main.tf
+++ aws/examples/acm/main.tf
@@ -3,34 +3,34 @@
 #
 
 terraform {
-    required_version = "~>0.12.2"
+    required_version = "~>0.12.12"
 }
 
 provider "aws" {
     region                  = "us-east-1"
     profile                 = "default"
-    shared_credentials_file = "${pathexpand("~/.aws/credentials")}"
+    shared_credentials_file = pathexpand("/Users/captain/.aws/credentials")
 }
 
 module "acm_certificate" {
     source                      = "../../modules/acm"
                         
-    acm_certificate             = "true"
+    enable_acm_certificate      = true
 
     domain_name                 = "linux-notes.org"
     subject_alternative_names   = ["www.linux-notes.org", "linux-notes.org"]
     validation_method           = "EMAIL"
 
-    import_existing_certificate = "false"
+    import_existing_certificate = false
     #private_key                 = ""
     #certificate_body            = ""
     #certificate_chain           = ""
 }
 
 module "acm_certificate_validation" {
-    source                      = "../../modules/acm"
+    source                              = "../../modules/acm"
     
-    acm_certificate_validation  = "true"
-    certificate_arn             = "${module.acm_certificate.aws_acm_certificate_arn}"
-    validation_record_fqdns     = []
+    enable_acm_certificate              = true
+    certificate_arn                     = "${module.acm_certificate.aws_acm_certificate_arn}"
+    validation_record_fqdns             = "linux-notes.org"
 }
