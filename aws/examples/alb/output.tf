9d4eca9c769c2c1df9471b200597e9a0af32ad9a
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-11-20 15:58:36 +0200

    Rewrite module to support TF-0.12

---
+++ aws/examples/alb/output.tf
@@ -0,0 +1,9 @@
+output "alb_name" {
+    description = "LB name"
+    value       = module.alb.lb_name
+}
+
+output "alb_tg_name" {
+    description = "LB target group name"
+    value       = module.alb.lb_tg_name
+}
