9acc629fb4c5cfe181ce21f948bbac0414e55e60
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-11-28 01:10:22 +0200

    TF-0.12 supporting

--- aws/examples/ec2/main.tf
+++ aws/examples/ec2/main.tf
@@ -2,80 +2,28 @@
 # MAINTAINER Vitaliy Natarov "vitaliy.natarov@yahoo.com"
 #
 terraform {
-  required_version = "> 0.9.0"
+    required_version = "> 0.12.12"
 }
+
 provider "aws" {
-    region  = "us-east-1"
+    region  = "us-west-2"
     profile = "default"
 }
-module "iam" {
-    source                          = "../../modules/iam"
-    name                            = "TEST-AIM"
-    region                          = "us-east-1"
-    environment                     = "PROD"
 
-    aws_iam_role-principals         = [
-        "ec2.amazonaws.com",
-    ]
-    aws_iam_policy-actions           = [
-        "cloudwatch:GetMetricStatistics",
-        "logs:DescribeLogStreams",
-        "logs:GetLogEvents",
-        "elasticache:Describe*",
-        "rds:Describe*",
-        "rds:ListTagsForResource",
-        "ec2:DescribeAccountAttributes",
-        "ec2:DescribeAvailabilityZones",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeVpcs",
-        "ec2:Owner",
-    ]
-}
-module "vpc" {
-    source                              = "../../modules/vpc"
-    name                                = "TEST-VPC"
-    environment                         = "PROD"
-    # VPC
-    instance_tenancy                    = "default"
-    enable_dns_support                  = "true"
-    enable_dns_hostnames                = "true"
-    assign_generated_ipv6_cidr_block    = "false"
-    enable_classiclink                  = "false"
-    vpc_cidr                            = "172.31.0.0/16"
-    private_subnet_cidrs                = ["172.31.64.0/20"]
-    public_subnet_cidrs                 = ["172.31.0.0/20"]
-    availability_zones                  = ["us-east-1a", "us-east-1b"]
-    enable_all_egress_ports             = "true"
-    allowed_ports                       = ["8080", "3306", "80", "443"]
-
-    map_public_ip_on_launch             = "true"
-
-    #Internet-GateWay
-    enable_internet_gateway             = "true"
-    #NAT
-    enable_nat_gateway                  = "false"
-    single_nat_gateway                  = "true"
-    #VPN
-    enable_vpn_gateway                  = "false"
-    #DHCP
-    enable_dhcp_options                 = "false"
-    # EIP
-    enable_eip                          = "false"
-}
 module "ec2" {
     source                              = "../../modules/ec2"
     name                                = "TEST-Machine"    
-    region                              = "us-east-1"
-    environment                         = "PROD"
-    ec2_instance_type                   = "t2.micro"
-    enable_associate_public_ip_address  = "true"
-    disk_size                           = "8"
-    tenancy                             = "${module.vpc.instance_tenancy}"
-    iam_instance_profile                = "${module.iam.instance_profile_id}"
-    subnet_id                           = "${element(module.vpc.vpc-publicsubnet-ids, 0)}"
-    #subnet_id                           = "${element(module.vpc.vpc-privatesubnet-ids, 0)}"
-    #subnet_id                           = ["${element(module.vpc.vpc-privatesubnet-ids)}"]
-    vpc_security_group_ids              = ["${module.vpc.security_group_id}"]
+    region                              = "us-west-2"
 
-    monitoring                          = "true"
+    enable_instance                     = true
+    environment                         = "dev"
+    instance_type                   = "t2.micro"
+    enable_associate_public_ip_address  = true
+    disk_size                           = 8
+    tenancy                             = "default"
+    iam_instance_profile                = ""
+    subnet_id                           = ""
+    vpc_security_group_ids              = []
+
+    monitoring                          = true
 }
--- aws/modules/ec2/variables.tf
+++ aws/modules/ec2/variables.tf
@@ -2,13 +2,13 @@
 # Global or/and default variables
 #-----------------------------------------------------------
 variable "name" {
-  description = "Name to be used on all resources as prefix"
-  default     = "TEST-EC2"
+    description = "Name to be used on all resources as prefix"
+    default     = "TEST-EC2"
 }
 
 variable "region" {
-  description = "The region where to deploy this code (e.g. us-east-1)."
-  default     = "us-east-1"
+    description = "The region where to deploy this code (e.g. us-east-1)."
+    default     = "us-east-1"
 } 
 
 variable "environment" {
@@ -24,24 +24,42 @@ variable "orchestration" {
 variable "createdby" {
     description = "Created by"
     default     = "Vitaliy Natarov"
-}    
+}
+
+variable "tags" {
+    description = "A list of tag blocks. Each element should have keys named key, value, and propagate_at_launch."
+    type        = map(string)
+    default     = {}
+}
+
+#-----------------------------------------------------------
+# AWS EC2
+#-----------------------------------------------------------
+variable "enable_instance" {
+    description   = "Enable instance usage"
+    default       = false
+}
+
+variable "instance_name" {
+    description   = "Name for AWS EC2 node(s)"
+    default       = ""
+}
 
 variable "ami" {
     description = "I added only 3 regions to show the map feature but you can add all"    
-    type        = "map"
     default     = {
         us-east-1 = "ami-46c1b650"
-        us-west-2 = "ami-46c1b650"
+        us-west-2 = "ami-50d1d929"
         eu-west-1 = "ami-6e28b517"
     }
 }
 
-variable "number_of_instances" {
-    description = "Number of instances to make"
-    default     = "1"
+variable "availability_zone" {
+    description   = "(Optional) The AZ to start the instance in."
+    default       = ""
 }
 
-variable "ec2_instance_type" {
+variable "instance_type" {
     description = "Type of instance t2.micro, m1.xlarge, c1.medium etc"
     default     = "t2.micro"
 }
@@ -52,23 +70,38 @@ variable "disk_size" {
 }
 
 variable "tenancy" {
-  description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
-  default     = "default"
+    description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
+    default     = "default"
+}
+
+variable "host_id" {
+    description   = "(optional) The Id of a dedicated host that the instance will be assigned to. Use when an instance is to be launched on a specific dedicated host."
+    default       = ""
+}
+
+variable "cpu_core_count" {
+    description   = "(Optional) Sets the number of CPU cores for an instance. This option is only supported on creation of instance type that support CPU Options CPU Cores and Threads Per CPU Core Per Instance Type - specifying this option for unsupported instance types will return an error from the EC2 API."
+    default       = 0
+}
+
+variable "cpu_threads_per_core" {
+    description   = "(Optional - has no effect unless cpu_core_count is also set) If set to to 1, hyperthreading is disabled on the launched instance. Defaults to 2 if not set. See Optimizing CPU Options for more information."
+    default       = 2
 }
 
 variable "ebs_optimized" {
-  description = "If true, the launched EC2 instance will be EBS-optimized"
-  default     = false
+    description = "If true, the launched EC2 instance will be EBS-optimized"
+    default     = false
 }
 
 variable "disable_api_termination" {
-  description = "If true, enables EC2 Instance Termination Protection"
-  default     = false
+    description = "If true, enables EC2 Instance Termination Protection"
+    default     = false
 }
 
 variable "instance_initiated_shutdown_behavior" {
-  description = "Shutdown behavior for the instance" # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/terminating-instances.html#Using_ChangingInstanceInitiatedShutdownBehavior
-  default     = ""
+    description = "Shutdown behavior for the instance"
+    default     = ""
 }
 
 variable "key_name" {
@@ -76,34 +109,34 @@ variable "key_name" {
     default     = ""
 }
 
-variable "public_key" {
-    description = "Key path to your RSA|DSA key"
-    default     = "/Users/captain/.ssh/id_rsa.pub"
+variable "get_password_data" {
+    description = "(Optional) If true, wait for password data to become available and retrieve it. Useful for getting the administrator password for instances running Microsoft Windows. The password data is exported to the password_data attribute. See GetPasswordData for more information."
+    default     = false
 }
 
-variable "private_key" {
-    description = "Private key"
-    default     = "/Users/captain/.ssh/id_rsa"
+variable "security_groups" {
+    description   = "(Optional, EC2-Classic and default VPC only) A list of security group names (EC2-Classic) or IDs (default VPC) to associate with."
+    default       = []
 }
 
 variable "enable_associate_public_ip_address" {
     description = "Enabling associate public ip address (Associate a public ip address with an instance in a VPC)"
-    default     = "true"
+    default     = true
 }
 
 variable "source_dest_check" {
     description = " (Optional) Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs. Defaults true."
-    default     = "false"
+    default     = false
 }
 
 variable "subnet_id" {
     description = "Public subnet ID"
-    #type        = "list"
+    default     = ""
 }
 
 variable "vpc_security_group_ids" {
     description = " SG for EC2"
-    type        = "list"
+    default     = []
 }
 
 variable "monitoring" {
@@ -115,6 +148,11 @@ variable "user_data" {
     description = "The user data to provide when launching the instance"
     default     = ""
 }
+
+#variable "user_data_base64" {
+#  description   = "(Optional) Can be used instead of user_data to pass base64-encoded binary data directly. Use this instead of user_data whenever the value is not a valid UTF-8 string. For example, gzip-encoded user data must be base64-encoded and passed via this argument to avoid corruption."
+#  default       = ""
+#}
 
 variable "iam_instance_profile" {
     description = "The IAM Instance Profile to launch the instance with. Specified as the name of the Instance Profile."
@@ -143,6 +181,7 @@ variable "ipv6_addresses" {
 
 variable "volume_tags" {
     description = "A mapping of tags to assign to the devices created by the instance at launch time"
+    type        = map(string)
     default     = {}
 }
 
@@ -164,4 +203,19 @@ variable "ephemeral_block_device" {
 variable "network_interface" {
     description = "Customize network interfaces to be attached at instance boot time"
     default     = []
+}
+
+variable "timeouts_create" {
+    description   = "(Defaults to 10 mins) Used when launching the instance (until it reaches the initial running state)"
+    default       = "10m"
+}
+
+variable "timeouts_update" {
+    description   = "(Defaults to 10 mins) Used when stopping and starting the instance when necessary during update - e.g. when changing instance type"
+    default       = "10m"
+}
+
+variable "timeouts_delete" {
+    description   = "(Defaults to 20 mins) Used when terminating the instance"
+    default       = "20m"
 }
