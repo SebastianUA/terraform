838a00218b67656fb22e5f4001eabde26e609f6c
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2019-12-01 00:00:41 +0200

    fixes

--- aws/modules/ecs/ecs_task_definition.tf
+++ aws/modules/ecs/ecs_task_definition.tf
@@ -1,74 +1,59 @@
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-    count                   = "${var.enable_ecs_task_definition && !var.enable_appmesh_proxy ? 1 :0 }"
+    count                   = var.enable_ecs_task_definition && !var.enable_appmesh_proxy ? 1 :0 
         
-    family                  = "${var.family !="" ? var.family : "${lower(var.name)}-service-${lower(var.environment)}" }"
-    #container_definitions   = "${file("${var.container_definitions}")}"
-    container_definitions   = "${var.container_definitions}"
+    family                  = var.family !="" ? var.family : "${lower(var.name)}-ecs-td-${lower(var.environment)}"
+    container_definitions   = var.container_definitions
     
-    task_role_arn           = "${var.task_role_arn}"
-    execution_role_arn      = "${var.execution_role_arn}"
-    network_mode            = "${var.network_mode}"
-    ipc_mode                = "${var.ipc_mode}"
-    pid_mode                = "${var.pid_mode}"
+    task_role_arn           = var.task_role_arn
+    execution_role_arn      = var.execution_role_arn
+    network_mode            = var.network_mode
+    ipc_mode                = var.ipc_mode
+    pid_mode                = var.pid_mode
 
-    volume {
-        name      = "${var.volume_name !="" ? var.volume_name : "${lower(var.name)}-volume-${lower(var.environment)}" }"
-        host_path = "${var.volume_host_path}"
-    }
-    #docker_volume_configuration {
-    #    scope           = "${var.docker_volume_configuration_scope}"
-    #    autoprovision   = "${var.docker_volume_configuration_autoprovision}"
-    #    driver          = ""
-    #    driver_opts     = []
-    #    labels          = []
-    #}
-
-    placement_constraints {
-        type       = "${var.task_definition_placement_constraints_type}"
-        expression = "${var.task_definition_placement_constraints_expression}"
-    }
-
-    tags {
-        Name                = "${var.family !="" ? var.family : "${lower(var.name)}-service-${lower(var.environment)}" }"
-        Environment         = "${var.environment}"
-        Region              = "${var.region}"    
-        Orchestration       = "${var.orchestration}"
-        Createdby           = "${var.createdby}"
-    }
-
-    lifecycle {
-        create_before_destroy   = true
-        ignore_changes          = []
-    }
-
-    depends_on = []
-}
-
-resource "aws_ecs_task_definition" "ecs_task_definition_appmesh_proxy" {
-    count                   = "${var.enable_ecs_task_definition && var.enable_appmesh_proxy ? 1 :0 }"
-
-    family                  = "${var.family}"
-    container_definitions   = "${file("${var.container_definitions}")}"
-
-    proxy_configuration {
-        type           = "${var.proxy_configuration_type}"
-        container_name = "${var.proxy_configuration_container_name !="" ? var.proxy_configuration_container_name : "${lower(var.name)}-container-${lower(var.environment)}" }"
-        properties = {
-            AppPorts         = "${var.proxy_configuration_properties_appports}"
-            EgressIgnoredIPs = "${var.proxy_configuration_properties_egressignoredips}"
-            IgnoredUID       = "${var.proxy_configuration_properties_ignoreduid}"
-            ProxyEgressPort  = "${var.proxy_configuration_properties_proxyegressport}"
-            ProxyIngressPort = "${var.proxy_configuration_properties_proxyingressport}"
+    dynamic "volume" {
+        for_each = var.volume
+        content {
+            name        = lookup(volume.value, "name", null)
+            host_path   = lookup(volume.value, "host_path", null)
         }
     }
 
-    tags {
-        Name                = "${var.volume_name !="" ? var.volume_name : "${lower(var.name)}-volume-${lower(var.environment)}" }"
-        Environment         = "${var.environment}"
-        Region              = "${var.region}"    
-        Orchestration       = "${var.orchestration}"
-        Createdby           = "${var.createdby}"
+    dynamic "placement_constraints" {
+        for_each = var.td_placement_constraints
+        content {
+            type        = lookup(td_placement_constraints.value, "type", null)
+            expression  = lookup(td_placement_constraints.value, "expression", null)
+        }
     }
+
+    proxy_configuration {
+        type           = var.proxy_configuration_type
+        container_name = var.proxy_configuration_container_name !="" ? var.proxy_configuration_container_name : "${lower(var.name)}-container-${lower(var.environment)}" 
+        
+        properties = {
+            AppPorts         = var.proxy_configuration_properties_appports
+            EgressIgnoredIPs = var.proxy_configuration_properties_egressignoredips
+            IgnoredUID       = var.proxy_configuration_properties_ignoreduid
+            ProxyEgressPort  = var.proxy_configuration_properties_proxyegressport
+            ProxyIngressPort = var.proxy_configuration_properties_proxyingressport
+        }
+    }
+
+    tags = merge(
+        {
+            "Name"          = var.ecs_cluster_name != "" ? var.ecs_cluster_name : "${lower(var.name)}-ecs-td-${lower(var.environment)}"
+        },
+        {
+            "Environment"   = var.environment
+        },
+        {
+            "Orchestration" = var.orchestration
+        },
+        {
+            "Createdby"     = var.createdby
+        },
+        var.tags,
+    )
 
     lifecycle {
         create_before_destroy   = true
--- aws/modules/ecs/variables.tf
+++ aws/modules/ecs/variables.tf
@@ -12,18 +12,24 @@ variable "region" {
 } 
 
 variable "environment" {
-    description = "Environment for service"
-    default     = "STAGE"
+  description = "Environment for service"
+  default     = "STAGE"
 }
 
 variable "orchestration" {
-    description = "Type of orchestration"
-    default     = "Terraform"
+  description = "Type of orchestration"
+  default     = "Terraform"
 }
 
 variable "createdby" {
-    description = "Created by"
-    default     = "Vitaliy Natarov"
+  description = "Created by"
+  default     = "Vitaliy Natarov"
+}
+
+variable "tags" {
+  description = "A list of tag blocks. Each element should have keys named key, value, and propagate_at_launch."
+  type        = map(string)
+  default     = {}
 }
 
 #------------------------------------------------------------------
@@ -31,7 +37,7 @@ variable "createdby" {
 #------------------------------------------------------------------
 variable "enable_ecs_cluster" {
   description = "Enable ecs cluster"
-  default     = "false"
+  default     = false
 }
 
 variable "ecs_cluster_name" {
@@ -41,12 +47,100 @@ variable "ecs_cluster_name" {
 
 variable "setting" {
   description = "(Optional) Configuration block(s) with cluster settings. For example, this can be used to enable CloudWatch Container Insights for a cluster. The name - (Required) Name of the setting to manage. Valid values: containerInsights. The value - (Required) The value to assign to the setting. Value values are enabled and disabled."
-  type        = "list"
-  default     = [{
-    "name"  = "containerInsights",
-    "value" = "enabled"
-    }
-  ]
+  default     = []
+}
+
+#------------------------------------------------------------------
+# AWS ECS task definition
+#------------------------------------------------------------------
+variable "enable_ecs_task_definition" {
+  description = "Enable ecs task definition"
+  default     = false
+}
+
+variable "family" {
+  description = "(Required) A unique name for your task definition."
+  default     = ""
+}
+
+variable "container_definitions" {
+  description = "(Required) A list of valid container definitions provided as a single valid JSON document. Please note that you should only provide values that are part of the container definition document. For a detailed description of what parameters are available, see the Task Definition Parameters section from the official Developer Guide."
+  default     = ""
+}
+
+variable "task_role_arn" {
+  description = "(Optional) The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
+  default     = ""
+}
+
+variable "execution_role_arn" {
+  description = "(Optional) The Amazon Resource Name (ARN) of the task execution role that the Amazon ECS container agent and the Docker daemon can assume."
+  default     = ""
+}
+
+variable "network_mode" {
+  description = "(Optional) The Docker networking mode to use for the containers in the task. The valid values are none, bridge, awsvpc, and host."
+  default     = "none"
+}
+
+variable "ipc_mode" {
+  description = "(Optional) The IPC resource namespace to be used for the containers in the task The valid values are host, task, and none."
+  default     = "none"
+}
+
+variable "pid_mode" {
+  description = "(Optional) The process namespace to use for the containers in the task. The valid values are host and task."
+  default     = "task"
+}
+
+variable "volume" {
+  description = "(Optional) A set of volume blocks that containers in your task may use."
+  default     = []
+}
+
+variable "td_placement_constraints" {
+  description = "(Optional) A set of placement constraints rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10."
+  default     = []
+}
+
+variable "proxy_configuration_type" {
+  description = "(Optional) The proxy type. The default value is APPMESH. The only supported value is APPMESH."
+  default     = null #"APPMESH"
+}
+
+variable "proxy_configuration_container_name" {
+  description = "(Required) The name of the container that will serve as the App Mesh proxy."
+  default     = ""
+}
+
+variable "proxy_configuration_properties_appports" {
+  description = ""
+  default     = null #8080
+}
+
+variable "proxy_configuration_properties_egressignoredips" {
+  description = ""
+  default     = null #"169.254.170.2,169.254.169.254"
+}
+
+variable "proxy_configuration_properties_ignoreduid" {
+  description = ""
+  default     = null #1339
+}
+
+variable "proxy_configuration_properties_proxyegressport" {
+  description = ""
+  default     = null #15001
+}
+
+variable "proxy_configuration_properties_proxyingressport" {
+  description = ""
+  default     = null #15000
+}
+
+variable "enable_appmesh_proxy" {
+  description = "Enable ecs task definition"
+  default     = false
 }
 
 #------------------------------------------------------------------
@@ -54,12 +148,12 @@ variable "setting" {
 #------------------------------------------------------------------
 variable "enable_ecs_service" {
   description = "Enable ecs service"
-  default     = "false"
+  default     = false
 }
 
 variable "enable_ecs_service_daemon" {
   description = "Enable ecs service as deamon"
-  default     = "false"
+  default     = false
 }
 
 variable "ecs_service_name" {
@@ -79,7 +173,7 @@ variable "task_definition" {
 
 variable "desired_count" {
   description = "(Optional) The number of instances of the task definition to place and keep running. Defaults to 0. Do not specify if using the DAEMON scheduling strategy."
-  default     = "3"
+  default     = 3
 }
 
 variable "iam_role" {
@@ -114,49 +208,27 @@ variable "deployment_maximum_percent" {
 
 variable "deployment_minimum_healthy_percent" {
   description = "(Optional) The lower limit (as a percentage of the service's desiredCount) of the number of running tasks that must remain running and healthy in a service during a deployment."
-  default     = "2"
+  default     = 2
 }
 
 variable "enable_ecs_managed_tags" {
   description = "(Optional) Specifies whether to enable Amazon ECS managed tags for the tasks within the service."
-  default     = "true"
+  default     = true
 }
 
 variable "health_check_grace_period_seconds" {
   description = "(Optional) Seconds to ignore failing load balancer health checks on newly instantiated tasks to prevent premature shutdown, up to 2147483647. Only valid for services configured to use load balancers."
-  default     = "2147483647"
+  default     = 2147483647
 }
 
-variable "network_configuration_subnets" {
-  description = "(Required) The subnets associated with the task or service."
-  type        = "list"
+variable "network_configuration" {
+  description = "(Optional) The network configuration for the service. This parameter is required for task definitions that use the awsvpc network mode to receive their own Elastic Network Interface, and it is not supported for other network modes."
   default     = []
 }
 
-variable "network_configuration_security_groups" {
-  description = "(Optional) The security groups associated with the task or service. If you do not specify a security group, the default security group for the VPC is used."
-  type        = "list"
+variable "service_registries" {
+  description = "(Optional) The service discovery registries for the service. The maximum number of service_registries blocks is 1."
   default     = []
-}
-
-variable "network_configuration_assign_public_ip" {
-  description = "(Optional) Assign a public IP address to the ENI (Fargate launch type only). Valid values are true or false. Default false."
-  default     = "false"
-}
-
-variable "service_registries_registry_arn" {
-  description = "(Required) The ARN of the Service Registry. The currently supported service registry is Amazon Route 53 Auto Naming Service(aws_service_discovery_service)."
-  default     = ""
-}
-
-variable "service_registries_port" {
-  description = "(Optional) The port value used if your Service Discovery service specified an SRV record."
-  default     = "9999"
-}
-
-variable "service_registries_container_port" {
-  description = "(Optional) The port value, already specified in the task definition, to be used for your service discovery service."
-  default     = "9999"
 }
 
 variable "service_registries_container_name" {
@@ -164,19 +236,14 @@ variable "service_registries_container_name" {
   default     = ""
 }
 
-variable "deployment_controller_type" {
-  description = "(Optional) Type of deployment controller. Valid values: CODE_DEPLOY, ECS. Default: ECS."
-  default     = "ECS"
+variable "deployment_controller" {
+  description = "(Optional) Configuration block containing deployment controller configuration."
+  default     = []
 }
 
-variable "load_balancer_elb_name" {
-  description = "(Required for ELB Classic) The name of the ELB (Classic) to associate with the service."
-  default     = ""
-}
-
-variable "load_balancer_target_group_arn" {
-  description = "(Required for ALB/NLB) The ARN of the Load Balancer target group to associate with the service."
-  default     = ""
+variable "load_balancer" {
+  description = "(Optional) A load balancer block."
+  default     = []
 }
 
 variable "load_balancer_container_name" {
@@ -184,140 +251,12 @@ variable "load_balancer_container_name" {
   default     = ""
 }
 
-variable "load_balancer_container_port" {
-  description = "(Required) The port on the container to associate with the load balancer."
-  default     = "8080"
+variable "ordered_placement_strategy" {
+  description = "(Optional) Service level strategy rules that are taken into consideration during task placement. List from top to bottom in order of precedence. The maximum number of ordered_placement_strategy blocks is 5. Defined below."
+  default     = []
 }
 
-variable "ordered_placement_strategy_type" {
-  description = "(Required) The type of placement strategy. Must be one of: binpack, random, or spread"
-  default     = "binpack"
-}
-
-variable "ordered_placement_strategy_field" {
-  description = "(Optional) For the spread placement strategy, valid values are instanceId (or host, which has the same effect), or any platform or custom attribute that is applied to a container instance. For the binpack type, valid values are memory and cpu. For the random type, this attribute is not needed."
-  default     = "cpu"
-}
-
-variable "placement_constraints_type" {
-  description = "(Required) The type of constraint. The only valid values at this time are memberOf and distinctInstance."
-  default     = "memberOf"
-}
-
-variable "placement_constraints_expression" {
-  description = "(Optional) Cluster Query Language expression to apply to the constraint. Does not need to be specified for the distinctInstance type."
-  default     = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b, us-east-2c]"
-}
-
-#------------------------------------------------------------------
-# AWS ECS task definition
-#------------------------------------------------------------------
-variable "enable_ecs_task_definition" {
-  description = "Enable ecs task definition"
-  default     = "false"
-}
-
-variable "family" {
-  description = "(Required) A unique name for your task definition."
-  default     = "service"
-}
-
-variable "container_definitions" {
-  description = "(Required) A list of valid container definitions provided as a single valid JSON document. Please note that you should only provide values that are part of the container definition document. For a detailed description of what parameters are available, see the Task Definition Parameters section from the official Developer Guide."
-  default     = ""
-}
-
-variable "task_role_arn" {
-  description = "(Optional) The ARN of IAM role that allows your Amazon ECS container task to make calls to other AWS services."
-  default     = ""
-}
-
-variable "execution_role_arn" {
-  description = "(Optional) The Amazon Resource Name (ARN) of the task execution role that the Amazon ECS container agent and the Docker daemon can assume."
-  default     = ""
-}
-
-variable "network_mode" {
-  description = "(Optional) The Docker networking mode to use for the containers in the task. The valid values are none, bridge, awsvpc, and host."
-  default     = "none"
-}
-
-variable "ipc_mode" {
-  description = "(Optional) The IPC resource namespace to be used for the containers in the task The valid values are host, task, and none."
-  default     = "none"
-}
-
-variable "pid_mode" {
-  description = "(Optional) The process namespace to use for the containers in the task. The valid values are host and task."
-  default     = "task"
-}
-
-variable "volume_name" {
-  description = " (Required) The name of the volume. This name is referenced in the sourceVolume parameter of container definition in the mountPoints section."
-  default     = ""
-}
-
-variable "volume_host_path" {
-  description = "(Optional) The path on the host container instance that is presented to the container. If not set, ECS will create a nonpersistent data volume that starts empty and is deleted after the task has finished."
-  default     = "/ecs/service-storage"
-}
-
-variable "docker_volume_configuration_scope" {
-  description = "(Optional) The scope for the Docker volume, which determines its lifecycle, either task or shared. Docker volumes that are scoped to a task are automatically provisioned when the task starts and destroyed when the task stops. Docker volumes that are scoped as shared persist after the task stops."
-  default     = "shared"
-}
-
-variable "docker_volume_configuration_autoprovision" {
-  description = "(Optional) If this value is true, the Docker volume is created if it does not already exist. Note: This field is only used if the scope is shared."
-  default     = "true"
-}
-
-variable "task_definition_placement_constraints_type" {
-  description = "(Required) The type of constraint. Use memberOf to restrict selection to a group of valid candidates. Note that distinctInstance is not supported in task definitions."
-  default     = "memberOf"
-}
-
-variable "task_definition_placement_constraints_expression" {
-  description = "(Optional) Cluster Query Language expression to apply to the constraint. For more information, see Cluster Query Language in the Amazon EC2 Container Service Developer Guide."
-  default     = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-}
-
-variable "proxy_configuration_type" {
-  description = "(Optional) The proxy type. The default value is APPMESH. The only supported value is APPMESH."
-  default     = "APPMESH"
-}
-
-variable "proxy_configuration_container_name" {
-  description = "(Required) The name of the container that will serve as the App Mesh proxy."
-  default     = ""
-}
-
-variable "proxy_configuration_properties_appports" {
-  description = ""
-  default     = "8080"
-}
-
-variable "proxy_configuration_properties_egressignoredips" {
-  description = ""
-  default     = "169.254.170.2,169.254.169.254"
-}
-
-variable "proxy_configuration_properties_ignoreduid" {
-  description = ""
-  default     = "1339"
-}
-
-variable "proxy_configuration_properties_proxyegressport" {
-  description = ""
-  default     = "15001"
-}
-
-variable "proxy_configuration_properties_proxyingressport" {
-  description = ""
-  default     = "15000"
-}
-
-variable "enable_appmesh_proxy" {
-  description = "Enable ecs task definition"
-  default     = "false"
+variable "service_placement_constraints" {
+  description = "(Optional) rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. Defined below."
+  default     = []
 }
