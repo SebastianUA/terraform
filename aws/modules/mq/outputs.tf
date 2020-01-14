c154dbfcb360014856b07b5391872fbcddd9b7a5
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2020-01-14 20:51:42 +0200

    Rewrite module to support 0.12 TF

--- aws/modules/mq/outputs.tf
+++ aws/modules/mq/outputs.tf
@@ -1,29 +1,35 @@
-output "mq_broker_ids" {
-    description = ""
-    value       = "${aws_mq_broker.mq_broker.*.id}"
+#---------------------------------------------------
+# AWS MQ broker configuration
+#---------------------------------------------------
+output "mq_configuration_id" {
+    description = "The unique ID that Amazon MQ generates for the configuration."
+    value       = element(concat(aws_mq_configuration.mq_configuration.*.id, [""]), 0)
 }
 
-output "mq_broker_arns" {
-    description = ""
-    value       = "${aws_mq_broker.mq_broker.*.arn}"
-}
-
-output "mq_broker_instances" {
-    description = ""
-    value       = "${aws_mq_broker.mq_broker.*.instances}"
-}
-
-output "mq_configuration_ids" {
-    description = ""
-    value       = "${aws_mq_configuration.mq_configuration.*.id}"
-}
-
-output "mq_configuration_arns" {
-    description = ""
-    value       = "${aws_mq_configuration.mq_configuration.*.arn}"
+output "mq_configuration_arn" {
+    description = "The ARN of the configuration."
+    value       = element(concat(aws_mq_configuration.mq_configuration.*.arn, [""]), 0)
 }
 
 output "mq_configuration_latest_revision" {
-    description = ""
-    value       = "${aws_mq_configuration.mq_configuration.*.latest_revision}"
+    description = "The latest revision of the configuration."
+    value       = element(concat(aws_mq_configuration.mq_configuration.*.latest_revision, [""]), 0)
+}
+
+#---------------------------------------------------
+# AWS MQ broker
+#---------------------------------------------------
+output "mq_broker_id" {
+    description = "The unique ID that Amazon MQ generates for the broker."
+    value       = element(concat(aws_mq_broker.mq_broker.*.id, [""]), 0)
+}
+
+output "mq_broker_arn" {
+    description = "The ARN of the broker."
+    value       = element(concat(aws_mq_broker.mq_broker.*.arn, [""]), 0)
+}
+
+output "mq_broker_instances" {
+    description = "A list of information about allocated brokers (both active & standby)."
+    value       = concat(aws_mq_broker.mq_broker.*.instances, [""])
 }
