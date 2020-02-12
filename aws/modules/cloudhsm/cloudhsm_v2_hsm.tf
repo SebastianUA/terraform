b33a951958d0ff2d0ef8baa260dbbd93a31e2015
Vitaliy Natarov <solo.metalisebastian@gmail.com>
2020-02-12 22:38:38 +0200

    Develop module with TF-0.12 supporting

--- aws/modules/cloudhsm/cloudhsm_v2_hsm.tf
+++ aws/modules/cloudhsm/cloudhsm_v2_hsm.tf
@@ -1,4 +1,22 @@
+#---------------------------------------------------
+# AWS cloudhsm v2 hsm
+#---------------------------------------------------
+resource "aws_cloudhsm_v2_hsm" "cloudhsm_v2_hsm" {
+    count               = var.enable_cloudhsm_v2_hsm ? 1 : 0
 
-#
-# https://www.terraform.io/docs/providers/aws/r/cloudhsm_v2_hsm.html
-#
+    subnet_id           = var.cloudhsm_v2_hsm_subnet_id
+    cluster_id          = var.cloudhsm_v2_hsm_cluster_id != "" && !var.enable_cloudhsm_v2_cluster ? var.cloudhsm_v2_hsm_cluster_id : element(concat(aws_cloudhsm_v2_cluster.cloudhsm_v2_cluster.*.cluster_id, [""]), 0)
+    
+    availability_zone   = var.cloudhsm_v2_hsm_availability_zone != null ? var.cloudhsm_v2_hsm_availability_zone : null
+    ip_address          = var.cloudhsm_v2_hsm_ip_address != null ? var.cloudhsm_v2_hsm_ip_address : null
+
+    lifecycle {
+        create_before_destroy   = true
+        ignore_changes          = []
+    }
+
+    depends_on                  = [
+        aws_cloudhsm_v2_cluster.cloudhsm_v2_cluster
+    ]
+
+}
