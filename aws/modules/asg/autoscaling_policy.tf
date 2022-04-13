78c582145e9e07d0c87e1dd71d373bc84bc1901c
Vitaliy Natarov <captain@MacBook.local>
2022-04-13 22:48:49 +0200

    Add fix for ASG

--- aws/modules/asg/autoscaling_policy.tf
+++ aws/modules/asg/autoscaling_policy.tf
@@ -75,11 +75,200 @@ resource "aws_autoscaling_policy" "autoscaling_policy" {
     for_each = lookup(var.autoscaling_policy_stack[count.index], "predictive_scaling_configuration", [])
 
     content {
-      metric_specification = lookup(predictive_scaling_configuration.value, "metric_specification", null)
-
       max_capacity_breach_behavior = lookup(predictive_scaling_configuration.value, "max_capacity_breach_behavior", null)
       max_capacity_buffer          = lookup(predictive_scaling_configuration.value, "max_capacity_buffer", null)
       scheduling_buffer_time       = lookup(predictive_scaling_configuration.value, "scheduling_buffer_time", null)
+
+      mode = lookup(predictive_scaling_configuration.value, "mode", null)
+
+      dynamic "metric_specification" {
+        iterator = metric_specification
+        for_each = lookup(predictive_scaling_configuration.value, "metric_specification", [])
+
+        content {
+          target_value = lookup(metric_specification.value, "target_value", null)
+
+          dynamic "customized_capacity_metric_specification" {
+            iterator = customized_capacity_metric_specification
+            for_each = lookup(metric_specification.value, "customized_capacity_metric_specification", [])
+
+            content {
+              dynamic "metric_data_queries" {
+                iterator = metric_data_queries
+                for_each = lookup(customized_capacity_metric_specification.value, "metric_data_queries", [])
+
+                content {
+                  id = lookup(metric_data_queries.value, "id", null)
+
+                  expression  = lookup(metric_data_queries.value, "expression", null)
+                  label       = lookup(metric_data_queries.value, "label", null)
+                  return_data = lookup(metric_data_queries.value, "return_data", null)
+
+                  dynamic "metric_stat" {
+                    iterator = metric_stat
+                    for_each = lookup(metric_data_queries.value, "metric_stat", [])
+
+                    content {
+                      dynamic "metric" {
+                        iterator = metric
+                        for_each = lookup(metric_stat.value, "metric_stat", [])
+
+                        content {
+                          namespace   = lookup(metric.value, "namespace", null)
+                          metric_name = lookup(metric.value, "metric_name", null)
+
+                          dynamic "dimensions" {
+                            iterator = dimensions
+                            for_each = lookup(metric.value, "dimensions", [])
+
+                            content {
+                              name  = lookup(dimensions.value, "name", null)
+                              value = lookup(dimensions.value, "value", null)
+                            }
+                          }
+                        }
+                      }
+                      stat = lookup(metric_stat.value, "stat", null)
+                      unit = lookup(metric_stat.value, "unit", null)
+                    }
+                  }
+                }
+              }
+            }
+
+          }
+
+          dynamic "customized_load_metric_specification" {
+            iterator = customized_load_metric_specification
+            for_each = lookup(metric_specification.value, "customized_load_metric_specification", [])
+
+            content {
+              dynamic "metric_data_queries" {
+                iterator = metric_data_queries
+                for_each = lookup(customized_load_metric_specification.value, "metric_data_queries", [])
+
+                content {
+                  id = lookup(metric_data_queries.value, "id", null)
+
+                  expression  = lookup(metric_data_queries.value, "expression", null)
+                  label       = lookup(metric_data_queries.value, "label", null)
+                  return_data = lookup(metric_data_queries.value, "return_data", null)
+
+                  dynamic "metric_stat" {
+                    iterator = metric_stat
+                    for_each = lookup(customized_load_metric_specification.value, "metric_stat", [])
+
+                    content {
+                      dynamic "metric" {
+                        iterator = metric
+                        for_each = lookup(customized_load_metric_specification.value, "metric_stat", [])
+
+                        content {
+                          namespace   = lookup(metric.value, "namespace", null)
+                          metric_name = lookup(metric.value, "metric_name", null)
+
+                          dynamic "dimensions" {
+                            iterator = dimensions
+                            for_each = lookup(metric.value, "dimensions", [])
+
+                            content {
+                              name  = lookup(dimensions.value, "name", null)
+                              value = lookup(dimensions.value, "value", null)
+                            }
+                          }
+                        }
+                      }
+                      stat = lookup(metric_stat.value, "stat", null)
+                      unit = lookup(metric_stat.value, "unit", null)
+                    }
+                  }
+                }
+              }
+            }
+
+          }
+
+          dynamic "customized_scaling_metric_specification" {
+            iterator = customized_scaling_metric_specification
+            for_each = lookup(metric_specification.value, "customized_scaling_metric_specification", [])
+
+            content {
+              dynamic "metric_data_queries" {
+                iterator = metric_data_queries
+                for_each = lookup(customized_scaling_metric_specification.value, "metric_data_queries", [])
+
+                content {
+                  id = lookup(metric_data_queries.value, "id", null)
+
+                  expression  = lookup(metric_data_queries.value, "expression", null)
+                  label       = lookup(metric_data_queries.value, "label", null)
+                  return_data = lookup(metric_data_queries.value, "return_data", null)
+
+                  dynamic "metric_stat" {
+                    iterator = metric_stat
+                    for_each = lookup(customized_scaling_metric_specification.value, "metric_stat", [])
+
+                    content {
+                      dynamic "metric" {
+                        iterator = metric
+                        for_each = lookup(customized_scaling_metric_specification.value, "metric_stat", [])
+
+                        content {
+                          namespace   = lookup(metric.value, "namespace", null)
+                          metric_name = lookup(metric.value, "metric_name", null)
+
+                          dynamic "dimensions" {
+                            iterator = dimensions
+                            for_each = lookup(metric.value, "dimensions", [])
+
+                            content {
+                              name  = lookup(dimensions.value, "name", null)
+                              value = lookup(dimensions.value, "value", null)
+                            }
+                          }
+                        }
+                      }
+                      stat = lookup(metric_stat.value, "stat", null)
+                      unit = lookup(metric_stat.value, "unit", null)
+                    }
+                  }
+                }
+              }
+            }
+          }
+
+          dynamic "predefined_load_metric_specification" {
+            iterator = predefined_load_metric_specification
+            for_each = lookup(metric_specification.value, "predefined_load_metric_specification", [])
+
+            content {
+              predefined_metric_type = lookup(predefined_load_metric_specification.value, "predefined_metric_type", null)
+              resource_label         = lookup(predefined_load_metric_specification.value, "resource_label", null)
+            }
+          }
+
+          dynamic "predefined_metric_pair_specification" {
+            iterator = predefined_metric_pair_specification
+            for_each = lookup(metric_specification.value, "predefined_metric_pair_specification", [])
+
+            content {
+              predefined_metric_type = lookup(predefined_metric_pair_specification.value, "predefined_metric_type", null)
+              resource_label         = lookup(predefined_metric_pair_specification.value, "resource_label", null)
+            }
+          }
+
+          dynamic "predefined_scaling_metric_specification" {
+            iterator = predefined_scaling_metric_specification
+            for_each = lookup(metric_specification.value, "predefined_scaling_metric_specification", [])
+
+            content {
+              predefined_metric_type = lookup(predefined_scaling_metric_specification.value, "predefined_metric_type", null)
+              resource_label         = lookup(predefined_scaling_metric_specification.value, "resource_label", null)
+            }
+          }
+
+        }
+      }
     }
   }
 
