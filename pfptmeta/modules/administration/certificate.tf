8544fd8a041d26bcb198366b24dc72d23c8d6525
Vitaliy Natarov <captain@MacBook.local>
2022-04-19 21:08:39 +0200

    Add templates

--- pfptmeta/modules/administration/certificate.tf
+++ pfptmeta/modules/administration/certificate.tf
@@ -1,5 +1,11 @@
 #-----------------------------------------------------------
 # ProofPoint Meta certificate
 #-----------------------------------------------------------
+resource "pfptmeta_certificate" "certificate" {
+  count = var.enable_certificate ? 1 : 0
 
-# https://registry.terraform.io/providers/nsofnetworks/pfptmeta/latest/docs/resources/certificate
+  name = var.certificate_name != "" ? var.certificate_name : "${lower(var.name)}-certificate-${lower(var.environment)}"
+  sans = var.certificate_sans
+
+  description = var.certificate_description
+}
