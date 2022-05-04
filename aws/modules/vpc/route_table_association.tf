691a46bea5fbcc9a6b5ba29cd859a57629a8671e
Vitaliy Natarov <captain@MacBook.local>
2022-05-04 15:15:40 +0200

    Add fix

--- aws/modules/vpc/route_table_association.tf
+++ aws/modules/vpc/route_table_association.tf
@@ -6,7 +6,7 @@
 resource "aws_route_table_association" "private_route_table_associations" {
   count = length(var.private_subnet_cidrs)
 
-  subnet_id      = var.private_subnet_cidrs[count.index]
+  subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = element(aws_route_table.private_route_tables.*.id, count.index)
 
   depends_on = [
@@ -19,7 +19,7 @@ resource "aws_route_table_association" "private_route_table_associations" {
 resource "aws_route_table_association" "k8s_private_route_table_associations" {
   count = length(var.k8s_private_subnet_cidrs)
 
-  subnet_id      = var.k8s_private_subnet_cidrs[count.index]
+  subnet_id      = aws_subnet.k8s_private_subnets[count.index].id
   route_table_id = element(aws_route_table.k8s_private_route_tables.*.id, count.index)
 
   depends_on = [
@@ -32,7 +32,7 @@ resource "aws_route_table_association" "k8s_private_route_table_associations" {
 resource "aws_route_table_association" "public_route_table_associations" {
   count = length(var.public_subnet_cidrs)
 
-  subnet_id      = var.public_subnet_cidrs[count.index]
+  subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = element(aws_route_table.public_route_tables.*.id, count.index)
 
   depends_on = [
@@ -45,7 +45,7 @@ resource "aws_route_table_association" "public_route_table_associations" {
 resource "aws_route_table_association" "k8s_public_route_table_associations" {
   count = length(var.k8s_public_subnet_cidrs)
 
-  subnet_id      = var.k8s_public_subnet_cidrs[count.index]
+  subnet_id      = element(aws_subnet.k8s_public_subnets.*.id, count.index)
   route_table_id = element(aws_route_table.k8s_public_route_tables.*.id, count.index)
 
   depends_on = [
