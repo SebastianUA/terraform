3f1d8db559ded6cbecfafe10626365cf64ea6a41
Vitaliy Natarov <captain@MacBook.local>
2022-05-16 13:09:58 +0200

    Fix

--- aws/modules/apigatewayv2/apigatewayv2_route_response.tf
+++ aws/modules/apigatewayv2/apigatewayv2_route_response.tf
@@ -5,7 +5,7 @@ resource "aws_apigatewayv2_route_response" "apigatewayv2_route_response" {
   count = var.enable_apigatewayv2_route_response ? 1 : 0
 
   api_id             = var.apigatewayv2_route_response_api_id != "" ? var.apigatewayv2_route_response_api_id : (var.enable_apigatewayv2_api ? aws_apigatewayv2_api.apigatewayv2_api[count.index].id : null)
-  route_id           = var.apigatewayv2_route_response_route_id != "" ? var.apigatewayv2_route_response_route_id : (var.enable_apigatewayv2_api ? aws_apigatewayv2_route.apigatewayv2_route[count.index].id : null)
+  route_id           = var.apigatewayv2_route_response_route_id != "" ? var.apigatewayv2_route_response_route_id : (var.enable_apigatewayv2_route ? aws_apigatewayv2_route.apigatewayv2_route[count.index].id : null)
   route_response_key = var.apigatewayv2_route_response_route_response_key
 
   model_selection_expression = var.apigatewayv2_route_response_model_selection_expression
