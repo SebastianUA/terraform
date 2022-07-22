39d7f5ebd971a914ed5f81bbda3fe238a2a28637
Vitaliy Natarov <captain@MacBook.local>
2022-07-22 16:33:03 +0200

    Split stack to small ones

---
+++ azure/examples/database_mssql/main.tf
@@ -0,0 +1,58 @@
+#
+# MAINTAINER Vitaliy Natarov "vitaliy.natarov@yahoo.com"
+#
+
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
+  features {}
+
+  // subscription_id = ""
+  // tenant_id       = ""
+}
+
+module "base_resource_group" {
+  source = "../../modules/base"
+
+  enable_resource_group   = true
+  resource_group_name     = "res-group"
+  resource_group_location = "West Europe"
+
+  tags = tomap({
+    "Environment"   = "test",
+    "Createdby"     = "Vitaliy Natarov",
+    "Orchestration" = "Terraform"
+  })
+
+}
+
+module "database_mssql" {
+  source = "../../modules/database_mssql"
+
+  tags = tomap({
+    "Environment"   = "test",
+    "Createdby"     = "Vitaliy Natarov",
+    "Orchestration" = "Terraform"
+  })
+
+  depends_on = [
+    module.base_resource_group
+  ]
+}
