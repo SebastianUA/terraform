22a7b8d28c834f1d711a67952a97ae667248c776
Vitalii Natarov <solo.metalisebastian@gmail.com>
2023-03-28 14:29:48 +0200

    Update config_remediation_configuration.tf

--- aws/modules/config/config_remediation_configuration.tf
+++ aws/modules/config/config_remediation_configuration.tf
@@ -11,14 +11,37 @@ resource "aws_config_remediation_configuration" "config_remediation_configuratio
   resource_type  = var.config_remediation_configuration_resource_type
   target_version = var.config_remediation_configuration_target_version
 
+  automatic                  = var.config_remediation_configuration_automatic
+  maximum_automatic_attempts = var.config_remediation_configuration_maximum_automatic_attempts
+  retry_attempt_seconds      = var.config_remediation_configuration_retry_attempt_seconds
+
   dynamic "parameter" {
     iterator = parameter
     for_each = var.config_remediation_configuration_parameter
+
     content {
       name = lookup(parameter.value, "name", null)
 
       resource_value = lookup(parameter.value, "resource_value", null)
       static_value   = lookup(parameter.value, "static_value", null)
+      static_values  = lookup(parameter.value, "static_values", null)
+    }
+  }
+
+  dynamic "execution_controls" {
+    iterator = execution_controls
+    for_each = var.config_remediation_configuration_execution_controls
+
+    content {
+      dynamic "ssm_controls" {
+        iterator = ssm_controls
+        for_each = length(keys(lookup(execution_controls.value, "ssm_controls", {}))) > 0 ? [lookup(execution_controls.value, "ssm_controls", {})] : []
+
+        content {
+          concurrent_execution_rate_percentage  = lookup(ssm_controls.value, "concurrent_execution_rate_percentage", null)
+          error_percentage                      = lookup(ssm_controls.value, "error_percentage", null)
+        }
+      }
     }
   }
 
