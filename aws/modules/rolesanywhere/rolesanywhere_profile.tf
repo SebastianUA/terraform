0253e57b2c070df04c206a561f2bf511024e9b97
Vitalii Natarov <solo.metalisebastian@gmail.com>
2023-09-15 01:18:26 +0200

    Add files via upload

---
+++ aws/modules/rolesanywhere/rolesanywhere_profile.tf
@@ -0,0 +1,5 @@
+#---------------------------------------------------
+# AWS rolesanywhere profile
+#---------------------------------------------------
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rolesanywhere_profile
