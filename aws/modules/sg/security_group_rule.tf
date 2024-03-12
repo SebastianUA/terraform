6fd2ba8eed9ce746382a7d78d9c1e8b474855bc6
Vitaliy Natarov <captain@MacBook.local>
2024-03-12 13:04:39 +0100

    Add fix

--- aws/modules/sg/security_group_rule.tf
+++ aws/modules/sg/security_group_rule.tf
@@ -8,7 +8,7 @@ resource "aws_security_group_rule" "ingress_ports" {
   protocol          = lookup(var.ingress_ports_stack[count.index], "protocol", null)
   from_port         = lookup(var.ingress_ports_stack[count.index], "from_port", null)
   to_port           = lookup(var.ingress_ports_stack[count.index], "to_port", null)
-  security_group_id = lookup(var.ingress_ports_stack[count.index], "to_port", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
+  security_group_id = lookup(var.ingress_ports_stack[count.index], "security_group_id", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
 
   cidr_blocks              = lookup(var.ingress_ports_stack[count.index], "cidr_blocks", null)
   prefix_list_ids          = lookup(var.ingress_ports_stack[count.index], "prefix_list_ids", null)
@@ -34,7 +34,7 @@ resource "aws_security_group_rule" "egress_ports" {
   protocol          = lookup(var.egress_ports_stack[count.index], "protocol", null)
   from_port         = lookup(var.egress_ports_stack[count.index], "from_port", null)
   to_port           = lookup(var.egress_ports_stack[count.index], "to_port", null)
-  security_group_id = lookup(var.egress_ports_stack[count.index], "to_port", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
+  security_group_id = lookup(var.egress_ports_stack[count.index], "security_group_id", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
 
   cidr_blocks              = lookup(var.egress_ports_stack[count.index], "cidr_blocks", null)
   prefix_list_ids          = lookup(var.egress_ports_stack[count.index], "prefix_list_ids", null)
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "ingress_ports_all" {
   count = var.enable_sg_rule_ingress_ports_all && !var.enable_sg_rule_ingress_ports ? length(var.ingress_ports_all_stack) : 0
 
   type              = "ingress"
-  security_group_id = lookup(var.ingress_ports_all_stack[count.index], "to_port", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
+  security_group_id = lookup(var.ingress_ports_all_stack[count.index], "security_group_id", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
   from_port         = lookup(var.ingress_ports_all_stack[count.index], "from_port", 0)
   to_port           = lookup(var.ingress_ports_all_stack[count.index], "to_port", 0)
   protocol          = lookup(var.ingress_ports_all_stack[count.index], "protocol", -1)
@@ -83,7 +83,7 @@ resource "aws_security_group_rule" "egress_ports_all" {
   count = var.enable_sg_rule_egress_ports_all && !var.enable_sg_rule_egress_ports ? length(var.egress_ports_all_stack) : 0
 
   type              = "egress"
-  security_group_id = lookup(var.egress_ports_all_stack[count.index], "to_port", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
+  security_group_id = lookup(var.egress_ports_all_stack[count.index], "security_group_id", (var.enable_security_group ? element(concat(aws_security_group.security_group.*.id, [""]), 0) : null))
   from_port         = lookup(var.egress_ports_all_stack[count.index], "from_port", 0)
   to_port           = lookup(var.egress_ports_all_stack[count.index], "to_port", 0)
   protocol          = lookup(var.egress_ports_all_stack[count.index], "protocol", -1)
